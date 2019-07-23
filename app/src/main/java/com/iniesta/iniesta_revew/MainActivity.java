7517a54333ab2db9aa4fea0acfc947bd9edb77f6
Ranger12021999 <amanksharma123a@gmail.com>
2019-07-23 12:50:03 +0530

    Initial commit

---
+++ app/src/main/java/com/iniesta/iniesta_revew/MainActivity.java
@@ -0,0 +1,53 @@
+package com.iniesta.iniesta_revew;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    public Toolbar toolbar;
+    private ActionBarDrawerToggle t;
+
+    public DrawerLayout drawerLayout;
+
+
+    public NavigationView navigationView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        navigationView=findViewById(R.id.navigationView);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        t=new ActionBarDrawerToggle(this, drawerLayout,R.string.navigation_drawer_open,R.string.navigation_drawer_close );
+        drawerLayout.addDrawerListener(t);
+        t.syncState();ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Toast.makeText(MainActivity.this, "Hello", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+
+        });
+
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if(t.onOptionsItemSelected(item))
+            return true;
+
+        return super.onOptionsItemSelected(item);
+    }
+}
